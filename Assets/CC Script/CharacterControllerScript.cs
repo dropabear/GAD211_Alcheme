f56615e57a3162cc75de1ae5a2b999d07bf765a1
ATHENA_LAPTOP\athen <1033585@student.sae.edu.au>
2025-08-02 12:17:20 +1000

    Completed character controller, camera also now follows character.

---
+++ Assets/CC Script/CharacterControllerScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public float speed = 12f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    public CharacterController controller;
+    private Vector3 velocity;
+
+    public float gravity = -20f;
+    public float groundDistance = 0.4f;
+    private bool isGrounded;
+    public float jumpHeight = 2f;
+
+    void Start()
+    {
+        // If the variable "controller" is empty...
+        if (controller == null)
+        {
+            // ...then this searches the components on the gameobject and gets a reference to the CharacterController class
+            controller = GetComponent<CharacterController>();
+        }
+    }
+
+    void Update()
+    {
+        // These lines let the script rotate the player based on the mouse moving
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        // Get the Left/Right and Forward/Back values of the input being used (WASD, Joystick etc.)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Let the player jump if they are on the ground and they press the jump button
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        // Rotate the player based off those mouse values we collected earlier
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+
+        // This is stealing the data about the player being on the ground from the character controller
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // This fakes gravity!
+        velocity.y += gravity * Time.deltaTime;
+
+        // This takes the Left/Right and Forward/Back values to build a vector
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Finally, it applies that vector it just made to the character
+        controller.Move(move * speed * Time.deltaTime + velocity * Time.deltaTime);
+    }
+}
+
